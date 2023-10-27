c69dea0fa17c7aa7f52381894da94f3a1bbd32c4
neon-softhamster <gpvruss@gmail.com>
2023-10-27 13:16:51 +0300

    normalization features

--- main.py
+++ main.py
@@ -2,10 +2,26 @@ import pandas as pd
 import numpy as np
 
 
-def cutData(Data, from_to):
+def generate_normalization_curve(temperature, a, k):
+    normalization_curve = [0] * len(temperature)
+    if evoluteExponent:
+        temperature_interval = temperature[0] - temperature[len(temperature) - 1]
+        for i in range(len(temperature)):
+            exp_ak_fraction = 1 / (1 + np.exp(0.32 * (temperature[i] - 50)))
+            # exp_ak_fraction = (0 - 1) / temperature_interval * temperature[i] + ((1 + 0) - (0 - 1) / temperature_interval) / 2
+            exp_AK_fraction = 1 - exp_ak_fraction
+            normalization_curve[i] = exp_ak_fraction * (np.exp(a) * np.exp(k * temperature[i])) + exp_AK_fraction * (np.exp(A) * np.exp(K * temperature[i]))
+    else:
+        for i in range(len(temperature)):
+            normalization_curve[i] = np.exp(a) * np.exp(k * temperature[i])
+    return normalization_curve
+
+
+def cut_data(data, from_to):
     T_interval = [int((from_to[0] - T[0]) / abs(T[1] - T[0]) + 2), int((from_to[1] - T[0]) / abs(T[1] - T[0]) + 2)]
-    cuttedData = Data[T_interval[0]:T_interval[1]]
+    cuttedData = data[T_interval[0]:T_interval[1]]
     return cuttedData
+
 
 def dispersion(a, b, Y, T):
     disp = 0
@@ -15,42 +31,6 @@ def dispersion(a, b, Y, T):
 
 
 if __name__ == '__main__':
-    # ------------------------------------------------------------------------------------------------------
-    # This program allow to find out "BckgIntensity" in two ways with normalization factors:
-    # 1.    N1(A, K, T) = 1 / (exp(A) * exp(K*T) + BckgIntensity)
-    # 2.    N2(A, K, T) = 1 / (freeDyeSignal + BckgIntensity)
-    # So the given section of the normalized function (Melt_Curve(T) + BckgIntensity)/Nx((A, K), T) at temperatures
-    # higher than T_cut_less is horizontal.
-    # !!! Locate your raw files in ../Raw folder !!!
-    # ------------------------------------------------------------------------------------------------------
-
-    # Files notation
-    file_normSignal = 'cy3-T20-bhq2'
-    # avaliable: 		cy3
-    #			cy3-T20-bhq2
-    #			ads14-ads14
-
-    file_signal = 'ATPaptACmm + ads8-14'
-    sheet_name_body = 'pH '
-    sheet_name_num = [5.0, 8.5, 0.5]
-    col_name_body = 'ads'
-    col_name_num = [8, 14, 1]
-
-    # Task
-    normalizeToOne = True
-    normOnRealSignal = False
-    T_cut_from = 80
-    T_cut_to = 95
-
-    # Parameters exp(A) * exp(K*T) comes from experimental measurements of Cy3T8 intensity on temperature
-    A = 12.70776
-    K = -0.0371940
-
-    # Range of background intensities checked sequentially with step (use int)
-    maxBckgIntensity = 50000
-    minBckgIntensity = -10000
-    step = 20
-
     with open('task.txt') as f:
         lines = f.readlines()
     for i in range(len(lines)):
@@ -61,21 +41,35 @@ if __name__ == '__main__':
         if normOnRealSignal:
             normSignal = excel_normSig_data['average'].tolist()
         else:
-            T_norm = excel_normSig_data['T'].tolist()
-            nbOfTPoints = len(T_norm)
-            normSignal = [0] * nbOfTPoints
-            for i in range(nbOfTPoints):
-                normSignal[i] = np.exp(A) * np.exp(K * T_norm[i])
+            normSignal = generate_normalization_curve(excel_normSig_data['T'].tolist(), A, K)
 
         for m in np.arange(sheet_name_num[0], sheet_name_num[1] + sheet_name_num[2], sheet_name_num[2]):
             sheet_name = sheet_name_body + str(m)
             excel_data = pd.read_excel('Raw/' + file_signal + '.xlsx', sheet_name=sheet_name)
             T = excel_data['T'].tolist()
-            T_cut = cutData(T, [T_cut_from, T_cut_to])
+            T_cut = cut_data(T, [T_cut_from, T_cut_to])
 
             for p in np.arange(col_name_num[0], col_name_num[1] + col_name_num[2], col_name_num[2]):
+                # Get signal from specified file
                 Signal_col_name = col_name_body + str(p)
                 Signal = excel_data[Signal_col_name].tolist()
+
+                # Calculate normalization exponent if needed
+                if findExponentParameters:
+                    T_info = excel_data['T interval ' + Signal_col_name].tolist()
+                    if T_info[0] != 'None':
+                        T_exp_determ_from = T_info[0]
+                        T_exp_determ_to = T_info[1]
+                        T_exp_determ_cut = cut_data(T, [T_exp_determ_from, T_exp_determ_to])
+                        Signal_exp_determ_cut = cut_data(Signal, [T_exp_determ_from, T_exp_determ_to])
+                        for k in range(len(Signal_exp_determ_cut)):
+                            Signal_exp_determ_cut[k] = np.log(Signal_exp_determ_cut[k])
+                        startRegionLinParameters = np.polyfit(T_exp_determ_cut, Signal_exp_determ_cut, 1)
+                        a = startRegionLinParameters[1]
+                        k = startRegionLinParameters[0]
+                        normSignal = generate_normalization_curve(excel_normSig_data['T'].tolist(), a, k)
+                    else:
+                        pass
 
                 nb_arg = int((maxBckgIntensity - minBckgIntensity) / step)
                 # Lists of BckgIntensity 'B'
@@ -84,18 +78,18 @@ if __name__ == '__main__':
                 b = [0] * int(nb_arg)
 
                 # Cut region at the end of melting curve
-                normSignal_cut = cutData(normSignal, [T_cut_from, T_cut_to])
-                Signal_cut = cutData(Signal, [T_cut_from, T_cut_to])
+                normSignal_cut = cut_data(normSignal, [T_cut_from, T_cut_to])
+                Signal_cut = cut_data(Signal, [T_cut_from, T_cut_to])
                 Profile2check = [0] * len(T_cut)
 
                 for j in range(nb_arg):
                     for i in range(len(T_cut)):
                         Profile2check[i] = (Signal_cut[i] + j * step + minBckgIntensity) / (normSignal_cut[i])
 
-                    linParam = np.polyfit(T_cut, Profile2check, 1)
+                    meltedRegionLinParameters = np.polyfit(T_cut, Profile2check, 1)
                     B_values[j] = j * step + minBckgIntensity
-                    B_abs_values[j] = linParam[0] ** 2
-                    b[j] = linParam[1]
+                    B_abs_values[j] = meltedRegionLinParameters[0] ** 2
+                    b[j] = meltedRegionLinParameters[1]
                     # dispers = dispersion(param[0], param[1], col2check, T_cut)
 
                 indexOfLowestB = B_abs_values.index(min(B_abs_values))
